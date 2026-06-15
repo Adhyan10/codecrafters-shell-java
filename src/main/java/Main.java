5ff113ff16979800f76cd8db5fc443a73318072a
adhyangauli@gmail.com <you@example.com>
2026-06-16 00:55:13 +0530

    #iz3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        // System.out.print("$ ");
 
-       
-        
-        // String s = sc.nextLine();
-
-        // System.out.print(s + ": command not found");
-
-        while(true){
-             System.out.print("$ ");
-              Scanner sc = new Scanner(System.in);
-             String s = sc.nextLine();
-             if(s.equals("exit")){
+        while (true) {
+            System.out.print("$ ");
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+            if (s.equals("exit")) {
                 break;
-             }
+            }else if(s.equals("echo")){
+                System.out.println(s);
+            }
             System.out.println(s + ": command not found");
         }
     }
 }
- 
