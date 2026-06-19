101b939ed8883a9af9c33fc0dfb37a310c3a6a1e
adhyangauli@gmail.com <you@example.com>
2026-06-19 12:23:38 +0530

    #ip1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,82 @@
 import java.io.File;
 import java.util.Scanner;
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+        Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.print("$ ");
-            Scanner sc = new Scanner(System.in);
-            String s = sc.nextLine();
-            if (s.equals("exit")) {
-                break;
-            } else if (s.startsWith("echo ")) {
-                System.out.println(s.substring(5));
-            } else if (s.startsWith("type ")) {
-                String command = s.substring(5);
-                if (command.equals("type") || command.equals("exit") || command.equals("echo")) {
-                    System.out.println(command + " is a shell builtin");
-                } else {
-                     String path = System.getenv("PATH");
-                    boolean found = false;
+            String input = sc.nextLine();
 
-                    if (path != null) {
-                        String[] directories = path.split(File.pathSeparator);
-                        for (String dir : directories) {
-                            File file = new File(dir, command);
-                            if (file.isFile() && file.canExecute()) {
-                                System.out.println(command + " is " + file.getAbsolutePath());
-                                found = true;
-                                break;
-                            }
+            if (input.trim().isEmpty()) {
+                continue;
+            }
+
+            String[] inputArgs = input.trim().split("\\s+");
+            String command = inputArgs[0];
+
+            if (command.equals("exit")) {
+                break;
+            } else if (command.equals("echo")) {
+                System.out.println(
+                    String.join(" ", Arrays.copyOfRange(inputArgs, 1, inputArgs.length))
+                );
+            } else if (command.equals("type")) {
+                if (inputArgs.length > 1) {
+                    String cmdToType = inputArgs[1];
+
+                    if (isBuiltin(cmdToType)) {
+                        System.out.println(cmdToType + " is a shell builtin");
+                    } else {
+                        String path = getPath(cmdToType);
+
+                        if (path != null) {
+                            System.out.println(cmdToType + " is " + path);
+                        } else {
+                            System.out.println(cmdToType + ": not found");
                         }
                     }
-                    if (!found) {
-                        System.out.println(command + ": not found");
-                    }
+                }
+            } else {
+                String path = getPath(command);
+
+                if (path != null) {
+                    String[] processArgs = inputArgs.clone();
+                    processArgs[0] = path;
+
+                    ProcessBuilder pb = new ProcessBuilder(processArgs);
+                    pb.inheritIO();
+
+                    Process process = pb.start();
+                    process.waitFor();
+                } else {
+                    System.out.println(command + ": command not found");
                 }
             }
         }
+    }
+
+    private static boolean isBuiltin(String cmd) {
+        return cmd.equals("exit") || cmd.equals("echo") || cmd.equals("type");
+    }
+
+    private static String getPath(String command) {
+        String pathEnv = System.getenv("PATH");
+
+        if (pathEnv != null) {
+            String[] directories = pathEnv.split(File.pathSeparator);
+
+            for (String dir : directories) {
+                File file = new File(dir, command);
+
+                if (file.isFile() && file.canExecute()) {
+                    return file.getAbsolutePath();
+                }
+            }
+        }
+
+        return null;
     }
 }
